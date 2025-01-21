ece0400080902939e15020958e2c6e7c69ee0545
soyuz43 <kebekad673@proton.me>
2025-01-21 10:17:14 -0600

    added clickevent stuff and a ERD

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,16 +1,34 @@
 // Employees.js
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
 
 const employees = getEmployees()
+const orders = getOrders()
 
 export const Employees = () => {
     let html = "<ul>"
 
     for (const employee of employees) {
-        html += `<li>${employee.name}</li>`
+        html += `
+            <li id="employee--${employee.id}" class="employee">
+                ${employee.name}
+            </li>
+        `
     }
 
     html += "</ul>"
 
     return html
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const employeesList = document.querySelector(".details__employees ul")
+
+    employeesList.addEventListener("click", (event) => {
+        if (event.target.classList.contains("employee")) {
+            const employeeId = event.target.id.split("--")[1]
+            const employeeOrders = orders.filter((order) => order.employeeId == employeeId)
+            const productsSold = employeeOrders.length
+            alert(`${event.target.textContent} has sold ${productsSold} products`)
+        }
+    })
+})
